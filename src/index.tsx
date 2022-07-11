3f31eb469875788efe1dc6f8f157c6f0f012f548
Yanal-Sabbagh07 <Yanal-Sabbagh07@github.com>
2022-07-12 00:21:18 +0200

    add energenious configuration

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,20 @@ import { AuthProvider } from "react-oidc-context";
 import { AuthProviderProps } from "react-oidc-context";
 
 const oidcConfig: AuthProviderProps = {
-  authority: "https://accounts.google.com/",
-
-  client_id:
-    "1285332583-puist3lq14110bkrj8l5708s1gck2ujo.apps.googleusercontent.com",
+  authority: "https://apidemo.energenious.net",
+  client_id: "PvGxUR6pkj6oEJNtfevAPv8mKK9bmSi1pjt4WrAncJ4",
+  client_secret: "S4TXncX0KykNic3XMlLQJXmdaUmRuJsDtH034nkY4YQ",
   redirect_uri: "https://localhost:3000/auth/callback",
   scope: "profile openid email",
   response_type: "code",
-  client_secret: "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",
+  metadata: {
+    authorization_endpoint: "https://api.energenious.net/oauth/authorize",
+    token_endpoint: "https://api.energenious.net/oauth/token",
+    revocation_endpoint: "https://api.energenious.net/oauth/revoke",
+    introspection_endpoint: "https://api.energenious.net/oauth/introspect",
+    userinfo_endpoint: "https://api.energenious.net/oauth/userinfo",
+    jwks_uri: "https://api.energenious.net/oauth/discovery/keys",
+  },
 };
 
 const root = ReactDOM.createRoot(
