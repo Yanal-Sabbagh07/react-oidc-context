f7014d688611445e9d3316132e86248737338172
abzokhattab <abdelrahman@energenious>
2022-08-09 14:03:39 +0200

    updating credentials

--- src/index.tsx
+++ src/index.tsx
@@ -8,18 +8,18 @@ import { AuthProviderProps } from "react-oidc-context";
 
 const oidcConfig: AuthProviderProps = {
   authority: "https://apidemo.energenious.net",
-  client_id: "PvGxUR6pkj6oEJNtfevAPv8mKK9bmSi1pjt4WrAncJ4",
-  client_secret: "S4TXncX0KykNic3XMlLQJXmdaUmRuJsDtH034nkY4YQ",
+  client_id: "RGWixnmLVuIpqEcz4pt56_B1xOaZZw0BpKJRRRrXGsI",
+  client_secret: "QjE6d1T5ZhSZP98Iu5ruj_mfexTWRO8zbH5L5AQQ2qo",
   redirect_uri: "https://localhost:3000/auth/callback",
   scope: "profile openid email",
   response_type: "code",
   metadata: {
-    authorization_endpoint: "https://api.energenious.net/oauth/authorize",
-    token_endpoint: "https://api.energenious.net/oauth/token",
-    revocation_endpoint: "https://api.energenious.net/oauth/revoke",
-    introspection_endpoint: "https://api.energenious.net/oauth/introspect",
-    userinfo_endpoint: "https://api.energenious.net/oauth/userinfo",
-    jwks_uri: "https://api.energenious.net/oauth/discovery/keys",
+    authorization_endpoint: "https://apidemo.energenious.net/oauth/authorize",
+    token_endpoint: "https://apidemo.energenious.net/oauth/token",
+    revocation_endpoint: "https://apidemo.energenious.net/oauth/revoke",
+    introspection_endpoint: "https://apidemo.energenious.net/oauth/introspect",
+    userinfo_endpoint: "https://apidemo.energenious.net/oauth/userinfo",
+    jwks_uri: "https://apidemo.energenious.net/oauth/discovery/keys",
   },
 };
 
